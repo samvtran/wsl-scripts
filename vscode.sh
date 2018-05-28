b1f246b34f3257c6666d0d46242f5fb6e75b6fca
Sam Tran <sam@samvtran.com>
2018-05-28 14:49:58 -0400

    Added note to unnecessary VS Code workaround

--- vscode.sh
+++ vscode.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+# NOTE: this probably isn't needed (anymore).
+# As long as <mount root>/Program Files/Microsoft VS Code/bin/code is accessible, `code` should pick up on paths correctly.
 #
 # Opens files and folders in a Windows-based Visual Studio Code install by passing Windows paths
-
+# 
 set -eu
 
 Path="$1"
